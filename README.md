224f5cd0fa1dfec7a6da790016a0ec8815e47443
DuctopwH <webstor.38@gmail.com>
2024-08-27 15:59:36 +0300

    Загружены файлы задания

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# airflow_minikube
-docker, minikube, airflow
+# Занятие 6.8. DataOps в Kubernetes с Apache Airflow
+
+## Задание. Создание python-скрипта в DAG
+
+Торшин Сергей
+
+Логи DAG с треугольником Паскаля на рисунке ниже.
+![](screenshot.jpg)
--- example_dag.py
+++ example_dag.py
@@ -1,24 +1,30 @@
 from airflow import DAG
-from airflow.operators.empty import EmptyOperator
+from airflow.operators.python_operator import PythonOperator
 from datetime import datetime
 
+def print_pascal_triangle():
+    triangle = []
+
+    for level in range(10):
+        row = [1] * (level+1)
+        for index in range(1, level):
+            row[index] = triangle[level-1][index-1] + triangle[level-1][index]
+        triangle.append(row)
+
+    for i in triangle:
+        for j in i:
+            print(j, end=' ')
+        print()
+
 default_args = {
-                'owner': 'airflow',
-                'depends_on_past': False,
-                'start_date': datetime(2023, 7, 19),
-                'email_on_failure': False,
-                'email_on_retry': False,
-               }
+    'owner': 'airflow',
+    'start_date': datetime(2024, 8, 27, 11, 44),
+}
 
-dag = DAG(
-          'example_dag',
-          default_args=default_args,
-          description='A simple tutorial DAG',
-          schedule=None,
-         )
+dag = DAG('pascal', default_args=default_args, schedule_interval='@daily')
 
-t1 = EmptyOperator(
-                   task_id='dummy_task',
-                   retries=3,
-                   dag=dag,
-                  )
+python_task = PythonOperator(
+    task_id='print_pascal_triangle',
+    python_callable=print_pascal_triangle,
+    dag=dag,
+)
